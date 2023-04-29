882ecefce1ed961d11a34672a59e9c082a68fcd3
Артём Токмаков <temtokmakov@yandex.ru>
2023-04-29 21:44:38 +0300

    third

--- homework.py
+++ homework.py
@@ -2,7 +2,6 @@ import logging
 import os
 import sys
 import time
-
 from http import HTTPStatus
 from json import JSONDecodeError
 
@@ -30,59 +29,50 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
-if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(asctime)s - %(levelname)s - %(name)s - '
-               '%(funcName)s - %(lineno)d - %(message)s',
-        handlers=[
-            logging.FileHandler('program.log', mode='w'),
-            logging.StreamHandler(sys.stdout)
-        ]
-    )
-
-logger = logging.getLogger(__name__)
-
 
 def check_tokens():
     """Проверка наличия токенов."""
-    no_token_msg = 'Программа принудительно остановлена.'
-    'Отсутствует переменная окружения: {}'
-    tokens = {
-        "PRACTICUM_TOKEN": PRACTICUM_TOKEN,
-        "TELEGRAM_TOKEN": TELEGRAM_TOKEN,
-        "TELEGRAM_CHAT_ID": TELEGRAM_CHAT_ID
-    }
-    for token, value in tokens.items():
-        if value is None:
-            logger.critical(no_token_msg.format(token))
-            return False
-    return True
+    missing_tokens = []
+    for key, value in {
+        'PRACTICUM_TOKEN': PRACTICUM_TOKEN,
+        'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
+        'TELEGRAM_CHAT_ID': TELEGRAM_CHAT_ID
+    }.items():
+        if not value:
+            missing_tokens.append(key)
+    if missing_tokens:
+        missing_tokens_list = ', '.join(missing_tokens)
+        logging.critical(f'Программа принудительно остановлена. '
+                         f'Отсутствуют переменные окружения: '
+                         f'{missing_tokens_list}')
+        return False
+    else:
+        return True
 
 
 def send_message(bot, message):
     """Отправка сообщения в Телеграм."""
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
-        logger.debug(f'Бот отправил сообщение: "{message}"')
+        logging.debug(f'Бот отправил сообщение: "{message}"')
     except telegram.error.TelegramError as error:
-        logger.error(f'Боту не удалось отправить сообщение: "{error}"')
+        logging.error(f'Боту не удалось отправить сообщение: "{error}"')
 
 
 def get_api_answer(timestamp):
     """Получение данных с API Яндекс Практикума."""
     timestamp = timestamp or int(time.time())
     params = {'from_date': timestamp}
-    logger.info(f'Запрос к API {ENDPOINT} с параметрами {params}')
+    logging.info(f'Запрос к API {ENDPOINT} с параметрами {params}')
     try:
         homework_statuses = requests.get(
             ENDPOINT,
             headers=HEADERS,
             params=params
         )
-        logger.info('Запрос выполнен успешно')
+        logging.info('Запрос выполнен успешно')
     except requests.RequestException as error:
-        message = ('Не удалось получить данные с {ENDPOINT} '
+        message = (f'Не удалось получить данные с {ENDPOINT} '
                    f'с параметрами {params}: {error}')
         raise ConnectionError(message)
     if homework_statuses.status_code != HTTPStatus.OK:
@@ -100,7 +90,7 @@ def get_api_answer(timestamp):
 
 def check_response(response):
     """Проверяем данные в response."""
-    logger.info('Начинаем проверку ответа API')
+    logging.info('Начинаем проверку ответа API')
     if not isinstance(response, dict):
         message = ('Тип данных в ответе от API не соответствует ожидаемому. '
                    f'Получен: {type(response)}')
@@ -114,18 +104,17 @@ def check_response(response):
         message = ('Список домашних заданий в ответе API приходит некорректно.'
                    f' Получен: {type(homeworks_list)}')
         raise TypeError(message)
-    logger.info('Проверка ответа API успешно завершена')
+    logging.info('Проверка ответа API успешно завершена')
     return homeworks_list
 
 
 def parse_status(homework):
     """Анализируем статус если изменился."""
-    logger.info(f'Начинаем анализ статуса работы '
-                f'"{homework.get("homework_name")}".')
+    logging.info('Начинаем анализ статуса работы '
+                 f'"{homework.get("homework_name")}".')
     homework_name = homework.get('homework_name')
     if not homework_name:
         message = 'Ключ homework_name недоступен в ответе API.'
-        logger.error(message)
         raise KeyError(message)
     homework_status = homework.get('status')
     if not homework_status:
@@ -136,7 +125,7 @@ def parse_status(homework):
                    f'"{homework_status}".')
         raise exceptions.ParseStatusException(message)
     verdict = HOMEWORK_VERDICTS[homework_status]
-    logger.info(f'Статус работы "{homework_name}" изменился на {verdict}.')
+    logging.info(f'Статус работы "{homework_name}" изменился на {verdict}.')
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -158,17 +147,27 @@ def main():
                 if last_message != homework_status:
                     last_message = homework_status
                     send_message(bot, homework_status)
-                    logger.info(homework_status)
+                    logging.info(homework_status)
             else:
-                logger.info('Статус не обновлен')
+                logging.info('Статус не обновлен')
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            logger.error(message, exc_info=True)
+            logging.error(message, exc_info=True)
             if last_message != message:
                 last_message = message
                 send_message(bot, message)
-        time.sleep(RETRY_PERIOD)
+        finally:
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - %(levelname)s - %(name)s - '
+               '%(funcName)s - %(lineno)d - %(message)s',
+        handlers=[
+            logging.FileHandler('program.log', mode='w'),
+            logging.StreamHandler(sys.stdout)
+        ]
+    )
     main()
